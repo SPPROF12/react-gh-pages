a4a4c6480386e5ad2822621e3ee1ab4453899d3e
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-12 21:57:49 +0100

    improve

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
+test('renders learn react link', async () => {
+  act(async () => {
+    render(<App />);
+  });
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
