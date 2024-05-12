614392306bf84a94b0c90bc083ba7c488c807eb6
SPPROF12 <159486580+SPPROF12@users.noreply.github.com>
2024-05-12 21:57:54 +0100

    improve

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,18 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [count, setCount] = React.useState(0);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          You clicked {count} times
         </p>
+        <button onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
         <a
           className="App-link"
           href="https://reactjs.org"
